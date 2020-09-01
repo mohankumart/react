0bb006f66c5fb70a94deb0ea48072a8b18f1cb7a
mohan.tadikimalla <mohan.tadikimalla@fireeye.com>
2020-09-01 23:04:37 +0530

    video app

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (event) => {
+        this.setState({
+            term: event.target.value,
+        });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSearch(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <label>Video Search</label>
+                    <input
+                        type="text"
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                    ></input>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
