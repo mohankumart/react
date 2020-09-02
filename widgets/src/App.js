1e44d4be26bdac01804257d098d3fd4b6ae5aa5c
mohan.tadikimalla <mohan.tadikimalla@fireeye.com>
2020-09-02 23:40:28 +0530

    drop down

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -17,10 +18,30 @@ const items = [
     },
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red',
+    },
+    {
+        label: 'The Color Green',
+        value: 'green',
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue',
+    },
+];
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div style={{ marginTop: '10px' }}>
-            <Search></Search>
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            ></Dropdown>
         </div>
     );
 };
