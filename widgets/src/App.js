5c52cf32951e686c48100504ad51f26a4665c60b
mohan.tadikimalla <mohan.tadikimalla@fireeye.com>
2020-09-03 00:34:05 +0530

    dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -35,13 +35,23 @@ const options = [
 
 const App = () => {
     const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
     return (
         <div style={{ marginTop: '10px' }}>
-            <Dropdown
-                selected={selected}
-                onSelectedChange={setSelected}
-                options={options}
-            ></Dropdown>
+            <button
+                onClick={() => {
+                    setShowDropdown(!showDropdown);
+                }}
+            >
+                Toggle Dropdown
+            </button>
+            {showDropdown ? (
+                <Dropdown
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    options={options}
+                ></Dropdown>
+            ) : null}
         </div>
     );
 };
